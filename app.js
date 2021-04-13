18830303a2ef7da226aa325db7e80a2e1737978b
ghalamdoost.shahab <ghalamdoost.shahab@live.com>
2021-04-13 19:09:05 -0400

    mongoosedb and dbContext created

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('./app_server/models/db');
+
+
 const indexRouter = require('./app_server/routes/index');
 
 var app = express();
--- app_server/controllers/fetch.js
+++ app_server/controllers/fetch.js
@@ -15,8 +15,24 @@ function getCurrentWeatherByCityId(cityId,callback){
         }
         callback(null, body);
     });
-} 
+}
+
+function getCurrentWeatherByLatLon(lat,lon,callback){
+    request({
+        url: url+'&lat='+lat+'&lon='+lon,
+        json: true
+    }, function(error, response, body){
+
+        if (error || response.statusCode !== 200) {
+            callback(error || {statusCode: response.statusCode});
+        }
+        callback(null, body);
+    });
+}
+
+
 
 module.exports={
-    getCurrentWeatherByCityId
+    getCurrentWeatherByCityId,
+    getCurrentWeatherByLatLon
 }
